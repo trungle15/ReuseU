793a6650bab908a62b344078c8ae66b8eb179e82
Howie Youngdahl <hosidyoungdahl@gmail.com>
2025-03-05 11:43:28 -0600

    dashboard

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+
+
+export default function ProfileDropdown() {
+    return (
+        <div className="flex items-center fixed top-0 left-0 w-full h-1/16 bg-green-500 z-50">
+            <button className= "w14"/>
+            <search
+            <h1>Dashboard</h1>
+        </div>
+    );
+  }
+  
