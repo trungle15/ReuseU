1c480fa4ba5ffc927bb930be5cf19f686aa44ca3
xVlad420x <petermurphygisb@yahoo.com>
2025-03-07 11:10:10 -0600

    database key added and boilerplate

--- backend/database-test.py
+++ backend/database-test.py
@@ -1,14 +1,21 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-service_account_path = "path/to/your/serviceAccountKey.json"
+#passes in creds to database and returns reference to root
 
-cred = credentials.Certificate(service_account_path)
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://reuseu-e42b8-default-rtdb.firebaseio.com/'
-})
+def get_db_root():
+    cred = credentials.Certificate("adminkey.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://reuseu-e42b8-default-rtdb.firebaseio.com/'
+    })
+    ref = db.reference('/')
+    return ref
 
-ref = db.reference('/')
-data = ref.get()
 
-print("Database content:", data)
+#prints all content from root of database
+def print_all_content():
+    ref = get_db_root()
+    data = ref.get()
+    print("Database content:", data)
+
+print_all_content()
