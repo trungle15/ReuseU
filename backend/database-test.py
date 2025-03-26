921b16ed12c104eb4e71c886a8d3db106442c1a0
xVlad420x <petermurphygisb@yahoo.com>
2025-03-25 20:35:35 -0500

    gitignore test

--- backend/database-test.py
+++ backend/database-test.py
@@ -1,10 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
+
 #passes in creds to database and returns reference to root
 
 def get_db_root():
-    cred = credentials.Certificate("adminkey.json")
+    cred = credentials.Certificate("peter_key.json")
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://reuseu-e42b8-default-rtdb.firebaseio.com/'
     })
@@ -15,7 +16,7 @@ def get_db_root():
 #prints all content from root of database
 def print_all_content():
     ref = get_db_root()
-    data = ref.get()
-    print("Database content:", data)
+    db_content = ref.get()
+    print("Database content:", db_content)
 
 print_all_content()
