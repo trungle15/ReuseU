245ba9b71db0bd9c1eead3b2168a5c2800b8cb3d
Howie Youngdahl <hosidyoungdahl@gmail.com>
2025-03-28 10:03:48 -0500

    added tag

--- frontend/src/components/Listings/Listing.tsx
+++ frontend/src/components/Listings/Listing.tsx
@@ -19,31 +19,31 @@ export default function Listing({ title, price, tags }: ListingProps) {
   return (
     //overall listing holder
     <div className="h-56 flex flex-row items-center justify-start text-center p-4 border rounded-lg gap-5">
-      <div className="h-full w-2/9 bg-gray-300 rounded-lg">
+      <div className="h-full w-2/9 border-2 border-double rounded-lg">
         {/*image goes here*/}
       </div>
-      <div className="h-full w-5/9 flex flex-col bg-gray-400 rounded-lg gap-[1vh] justify-center">
-        <div className ="h-1/8 bg-green-500 rounded-lg">
+      <div className="h-full w-5/9 flex flex-col border-2 border-dotted rounded-lg gap-[1vh] justify-center">
+        <div className ="h-1/8 border-2 border-double rounded-lg">
           <h3 className="font-bold">Title</h3>
         </div>
-        <div className ="h-1/4 bg-green-600 rounded-lg">
+        <div className ="h-1/4 border-2 border-doublerounded-lg">
           <text className="self-start ml-[1vh]">
             This is where my tags will go
           </text>
         </div>
-        <div className ="h-1/2 bg-green-700 rounded-lg">
+        <div className ="h-1/2 border-2 border-double rounded-lg">
         <text className="self-start ml-[1vh]">
             Bruh stinky stinky stinky bruh bruh stinky stinky stinky bruh bruh stink
           </text>
         </div>
       </div>
         
-      <div className="h-full w-2/9 bg-gray-500 rounded-lg flex flex-col rounded-lg justify-between">
+      <div className="h-full w-2/9 rounded-lg flex flex-col rounded-lg justify-between border-2 border-double">
         <div className="">
           $19.99
         </div>
-        {isFavorited && <HeartOutline onClick={onFavoriteClick} className="cursor-pointer h-1/4 -mr-[12vh]"></HeartOutline>}
-        {!isFavorited && <HeartSolid onClick={onFavoriteClick} className="cursor-pointer h-1/4 -mr-[12vh] "></HeartSolid>}
+        {isFavorited && <HeartOutline onClick={onFavoriteClick} className="cursor-pointer h-1/4 -mr-[12vh] fill-white"></HeartOutline>}
+        {!isFavorited && <HeartSolid onClick={onFavoriteClick} className="cursor-pointer h-1/4 -mr-[12vh] fill-red-500"></HeartSolid>}
       </div>
     </div>
   );
