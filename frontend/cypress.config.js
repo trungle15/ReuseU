384dbaacb1dc8559a00c167e9be9111ef0de47d9
Howie Youngdahl <hosidyoungdahl@gmail.com>
2025-03-29 12:09:37 -0500

    local ip used

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     supportFile: false, // no need for support file
-    baseUrl: 'http://localhost:3000', // url
+    baseUrl: 'http://132.161.162.101:3000', // url
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
