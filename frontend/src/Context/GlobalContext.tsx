e3bdb9e39cfcd66e0d5ed1db10198a5f98fb7f6c
Timur Kasimov <kasimovmoor@gmail.com>
2025-04-08 14:21:03 -0500

    added the feature to view the full listing on click

--- frontend/src/Context/GlobalContext.tsx
+++ frontend/src/Context/GlobalContext.tsx
@@ -1,12 +1,17 @@
 import { createContext, useContext, useState } from 'react'
+import Listing from '@/components/Listings/Listing'
+import { SAMPLE_LISTINGS } from '@/components/Listings/FullListing'
 
 const GlobalContext = createContext<any>(null)
 
 export const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
   const [filters, setFilters] = useState<Array<string>>([])
+  const [title, setTitle] = useState<string>('')
+  // const [listing, setListing] = useState<string>('')
+  const [listings, setListings] = useState<Array<any>>([])
 
   return (
-    <GlobalContext.Provider value={{ filters, setFilters}}>
+    <GlobalContext.Provider value={{ filters, setFilters, title, setTitle, listings, setListings}}>
       {children}
     </GlobalContext.Provider>
   )
